70c4372c116931959bccdab3aff5e21d91a863ec
¨javi <javis9555@gmail.com>
2024-02-05 18:13:42 +0100

    models

---
+++ db/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Contact(models.Model):
+    foto_perfil = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=9)
+    comentario = models.TextField()
+    archive = models.BooleanField()
+
+
+class Rooms(models.Model):
+    roomNumber = models.IntegerField()
+    img = models.CharField(max_length=255)
+    bedType = models.CharField(max_length=255)
+    facilities = models.JSONField()
+    rate = models.FloatField()
+    offerPrice = models.FloatField()
+    status = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=255)
+    
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    nota = models.TextField()
+    id_habitacion = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    status = models.CharField(max_length=255)
+    
+    
