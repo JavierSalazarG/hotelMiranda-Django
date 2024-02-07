e59107eb4b5441de7feaba10531ddb08a0b61256
javi <javis9555@gmail.com>
2024-02-07 20:36:35 +0100

    link imgs

--- hotelMiranda/urls.py
+++ hotelMiranda/urls.py
@@ -22,5 +22,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admindb/', admin.site.urls),
-    path('home/', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
