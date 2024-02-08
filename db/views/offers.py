f0ed62f4f943ec8e5ff948e49890cd8e4812242d
javi <javis9555@gmail.com>
2024-02-08 20:26:04 +0100

    offers stilos y llamada

--- db/views/offers.py
+++ db/views/offers.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import F
+from db.models import Rooms
 def offers(request):
+    discounted_rooms = Rooms.objects.filter(offerPrice__lt=F('rate'))
     return render(
         request,
         "../templates/offers.html",
+        {"rooms": discounted_rooms}
 )
